0b2169309ebd466e0fd11bc884249fa3157cbf63
Poorna Ranasinghe <onlinepoorna@gmail.com>
2022-08-08 10:18:53 +0530

    cart item increase function

--- src/reducer.js
+++ src/reducer.js
@@ -9,11 +9,31 @@ function reducer(state, action) {
   }
 
   if (action.type === DECREASE) {
-    console.log("DECREASE amount");
+    let tempCart = [];
+    if (action.payload.amount === 1) {
+      tempCart = state.cart.filter(
+        (cartItem) => cartItem.id !== action.payload.id
+      );
+    } else {
+      let tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      });
+    }
+
+    return { ...state, cart: tempCart };
   }
 
   if (action.type === INCREASE) {
-    console.log("INCREASE amount");
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload.id) {
+        cartItem = { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
   }
 
   if (action.type === REMOVE) {
